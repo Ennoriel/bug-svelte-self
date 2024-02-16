50b89c45dd82fd79bf720fff51821d68b0880e6f
Maxime Dupont <maxime.dupont@alcyconie.com>
2024-02-16 09:17:46 +0100

    first commit

---
+++ src/lib/Compo.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let child = false;
+	export let n: number;
+</script>
+
+<p>{n}</p>
+
+{#if child}
+	<!-- replacing svelte:self with svelte:component and importing the component itself works as expected -->
+	<svelte:self n="this should not be valid" />
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Compo from '$lib/Compo.svelte';
+</script>
+
+<Compo n={3} child />
